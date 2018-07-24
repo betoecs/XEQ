15dc2faca5470c64cc26a165ac28292932fc329a
Samantha Hernandez <milysam05@hotmail.com>
2018-07-24 08:18:42 -0500

    upload games

---
+++ js/upload-game-section.js
@@ -0,0 +1,8 @@
+var uploadGameSection =
+{
+    showUploadGameSection: function()
+	{
+		document.getElementById('upload-game-section').style.display = 'block';
+		document.getElementById('games-section').style.display = 'none';
+	},
+}
