b0f25571e80ffc43ea16b1ed464f865336f7d01c
Samantha Hernandez <milysam05@hotmail.com>
2018-07-24 12:20:56 -0500

    Allready image upload and start the js file upload

--- js/upload-game-section.js
+++ js/upload-game-section.js
@@ -1,8 +1,18 @@
 var uploadGameSection =
 {
-    showUploadGameSection: function()
+    show: function()
 	{
 		document.getElementById('upload-game-section').style.display = 'block';
 		document.getElementById('games-section').style.display = 'none';
 	},
+
+    setImage: function(uploadInput)
+    {
+        setImage(document.getElementById('game-image'), uploadInput, false);
+    },
+
+    setJSFile: function(uploadInput)
+    {
+        document.getElementById('game-js-file-upload').placeholder = uploadInput.value;
+    }
 }
